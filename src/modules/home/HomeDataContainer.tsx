3ff0bcafb2fc6134caa2898c9fec025ffbbce2bf
jesusquevedo <jesus.quevedo@cuatroochenta.com>
2020-10-29 17:23:47 +0100

    [ADDED] Logout message with Swal

--- src/modules/home/HomeDataContainer.tsx
+++ src/modules/home/HomeDataContainer.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { NavbarHome } from "../../components/joinder/ui/NavbarHome";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../reducers/rootReducer";
-
+import Swal from "sweetalert2";
 import "./HomeStyle.scss";
 import { JoinEventModal } from "../../components/modals/JoinEventModal";
 import { CreateEventModal } from "../../components/modals/CreateEventModal";
@@ -20,6 +20,7 @@ export const HomeDataContainer: React.FunctionComponent = () => {
   const { username } = useSelector((state: RootState) => state.auth);
   const handleLogout = () => {
     dispatch(logout());
+    Swal.fire("Sesion Cerrada", "Has cerrado sesion correctamente!", "success");
   };
 
   const handleCreateEvent = async (event: EventObject) => {
