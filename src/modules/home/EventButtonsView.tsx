44f730dab6309d3ee772b8e6efc63a6b97e29a59
jesusquevedo <jesus.quevedo@cuatroochenta.com>
2020-11-02 09:19:13 +0100

    [REFACTOR] View of buttons movel to individial file

---
+++ src/modules/home/EventButtonsView.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Button, Row, Col, Container } from "react-bootstrap";
+interface EventButtonsViewParams {
+  setModalShowJoin: (arg: boolean) => void;
+  setModalShowCreate: (arg: boolean) => void;
+}
+
+export const EventButtonsView = ({
+  setModalShowJoin,
+  setModalShowCreate,
+}: EventButtonsViewParams) => {
+  return (
+    <Container>
+      <Col>
+        <Row md={6} xs={12}>
+          <Button
+            variant="outline-secondary"
+            className="buttonadd"
+            size="lg"
+            onClick={() => {
+              setModalShowCreate(true);
+            }}
+          >
+            Crear evento
+          </Button>
+        </Row>
+      </Col>
+      <Col>
+        <Row md={6} xs={12}>
+          <Button
+            className="buttonjoin"
+            variant="outline-primary"
+            size="lg"
+            onClick={() => {
+              setModalShowJoin(true);
+            }}
+          >
+            Unirse a evento
+          </Button>
+        </Row>
+      </Col>
+    </Container>
+  );
+};
